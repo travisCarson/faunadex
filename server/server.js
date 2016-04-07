3bfbb3b287383867c3c01a7ab4dc25b52f94930a
Will Wheeler <willfulbard@gmail.com>
2016-04-07 14:32:43 -0700

    Fix error with routing and add webpack file to .gitignore

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ var app = express();
 var path = require('path');
 
 require('./config/middleware.js')(app, express);
-require('.config/routes.js')(app, express);
+require('./config/routes.js')(app, express);
 
 app.listen(1337);
+console.log('1337 server started!');
 
