bd7eea9015ae7da2f8acf07881be451325cecf14
Alexander James Turinske <a.turinske@gmail.com>
2016-04-07 16:46:36 -0700

    Complete routing and requires

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,20 +1,20 @@
 // var userController = require('../models/user/userController.js');
 var path = require('path');
-var userModel = require('');
-var encounterModel = require('');
+var userModel = require('../models/user');
+var encounterModel = require('../models/encounter');
 
 module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
 
   // user routing
-  app.post('/api/user/signup', userController.signUp);
-  app.post('/api/user/signin', userController.signIn);
-  app.post('/api/user/signout', userController.signOut);
+  app.post('/api/user/signup', userModel.signUp);
+  app.post('/api/user/signin', userModel.signIn);
+  app.post('/api/user/signout', userModel.signOut);
 
   // encounter routing
-  app.get('/api/user/encounter', encounterController.showAllEncounters);
-  app.post('/api/user/encounter', encounterController.createEncounter);
-  app.get('/api/recentactivity', encounterController.recentactivity);
+  app.get('/api/user/encounter', encounterModel.showAllEncounters);
+  app.post('/api/user/encounter', encounterModel.createEncounter);
+  app.get('/api/recentactivity', encounterModel.recentactivity);
 
   // app.post('/api/searchanimal', someController);
   // app.get('/api/friends/recentactivity', someController)
