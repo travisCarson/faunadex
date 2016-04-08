bd3bcf24746d1c96e6273963e95ef088f99a012d
Alexander James Turinske <a.turinske@gmail.com>
2016-04-07 21:07:24 -0700

    Modify routes to point to functions in utils files

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,19 +1,21 @@
 var path = require('path');
-var userModel = require('../models/user');
-var encounterModel = require('../models/encounter');
+var userUtils = require('./userUtils.js');
+var encounterUtils = require('./encounterUtils.js');
 
 module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
 
   // user routing
-  app.post('/api/user/signup', userModel.signUp);
-  app.post('/api/user/signin', userModel.signIn);
-  app.post('/api/user/signout', userModel.signOut);
+  app.post('/api/user/signup', userUtils.createUser);
+  // TODO check that createSession actually signs in a user
+  // I don't think it does. I don't think there is a signin function
+  app.post('/api/user/signin', userUtils.createSession);
+  app.post('/api/user/signout', userUtils.endSession);
 
   // encounter routing
-  app.get('/api/user/encounter', encounterModel.showAllEncounters);
-  app.post('/api/user/encounter', encounterModel.createEncounter);
-  app.get('/api/recentactivity', encounterModel.recentactivity);
+  app.get('/api/user/encounter', encounterUtils.showAllEncounters);
+  app.post('/api/user/encounter', encounterUtils.createEncounter);
+  app.get('/api/recentactivity', encounterUtils.recentactivity);
 
   // app.post('/api/searchanimal', someController);
   // app.get('/api/friends/recentactivity', someController)
