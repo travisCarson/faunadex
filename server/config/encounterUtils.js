99f59feb81991d3f5f98ce9761393288c78a5b6a
Alexander James Turinske <a.turinske@gmail.com>
2016-04-07 20:10:47 -0700

    Refactor createEncounter method to be easier to read

--- server/config/encounterUtils.js
+++ server/config/encounterUtils.js
@@ -9,23 +9,25 @@ var bookshelf = require('bookshelf');
 module.exports = {
 
   createEncounter: function() {
-    // TODO add description to schema.sql encounter table
     Encounters.create({
       title: req.body.title,
+      // TODO add description to schema.sql encounter table
+      description: req.body.description,
       location: req.body.location,
       encountertime: req.body.encountertime,
       posttime: new Date()
     })
-    .then(function(encounter) {
-      res.status(200).send(encounter);
-    })
-    .catch(function(error) {
-      res.status(500).send(error.message);
-    });
+      // send back a status code to signify success and the encounter for the front-end to use (if necessary)
+      .then(function(encounter) {
+        res.status(200).send(encounter);
+      })
+      .catch(function(error) {
+        res.status(500).send(error.message);
+      });
   },
 
   showAllEncounters: function() {
-
+    
   },
 
   recentActivity: function() {
