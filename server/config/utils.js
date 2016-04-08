b8e34ea64620e0a67632a87dbb52fe3470046dd2
Alexander James Turinske <a.turinske@gmail.com>
2016-04-07 19:00:41 -0700

    Add utils.js to hold all the bookshelf methods and helper methods

---
+++ server/config/utils.js
@@ -0,0 +1,21 @@
+var db = require('../config/db.js');
+var User = require('../models/user');
+var Encounter = require('../models/encounter');
+var knex = require('knex');
+var bookshelf = require('bookshelf');
+
+module.exports = {
+
+  createEncounter: function() {
+
+  },
+
+  showAllEncounters: function() {
+
+  },
+
+  recentActivity: function() {
+
+  }
+
+};
