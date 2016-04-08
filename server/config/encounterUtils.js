a2cc90a4dae7aaa73cf332883784e1b7752f65ec
Alexander James Turinske <a.turinske@gmail.com>
2016-04-07 20:11:51 -0700

    Implement showAllEncounters to fetch data from the database

--- server/config/encounterUtils.js
+++ server/config/encounterUtils.js
@@ -27,7 +27,13 @@ module.exports = {
   },
 
   showAllEncounters: function() {
-    
+    Encounters.fetch()
+      .then(function(encouters) {
+        res.status(200).send(encouters);
+      })
+      .catch(function(error) {
+        res.status(500).send(error.message);
+      });
   },
 
   recentActivity: function() {
