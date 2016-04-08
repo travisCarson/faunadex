1d87d8e7c933be311af934462840ba7787c187c2
Travis Carson <travislcarson@gmail.com>
2016-04-07 20:27:05 -0700

    fix commas in package.json, add colons in user.js

--- server/models/user.js
+++ server/models/user.js
@@ -7,12 +7,12 @@ var User = db.Model.extend({
   initialize: function() {
     this.on('creating', this.hashPassword);
   },
-  comparePassword = function(attemptedPassword, callback) {
+  comparePassword: function(attemptedPassword, callback) {
     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
       callback(isMatch);
     });
   },
-  hashPassword = function() {
+  hashPassword: function() {
     var cipher = Promise.promisify(bcrypt.hash);
     return cipher(this.get('password'), null, null).bind(this)
       .then(function(hash) {
