9a282f2b514cabd4c0a44102c8492824676cde6c
Hack Reactor Students <accounts@hackreactor.com>
2016-04-08 11:36:59 -0700

    Fix path to encounters collection

--- server/config/encounterUtils.js
+++ server/config/encounterUtils.js
@@ -2,7 +2,7 @@ var db = require('../config/db.js');
 var User = require('../models/user');
 var Users = require('../collections/users');
 var Encounter = require('../models/encounter');
-var Encounters = require('../models/encounters');
+var Encounters = require('../collections/encounters');
 var knex = require('knex');
 var bookshelf = require('bookshelf');
 
