8cee3b2fea8f6559109f3fa85d2c538719e2c2bb
Hack Reactor Students <accounts@hackreactor.com>
2016-04-08 12:29:22 -0700

    Attempt to implement database connection from a tutorial

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,24 @@
 var knex = require('knex')({
   client: 'pg',
   connection: {
+    user: 'postgres', //tbd
+    database: 'faunadex-db',
+    port: 5432,
     host: 'localhost',
-    user: '', //tbd
-    password: '', //tbd
-    database: '', //faunadex-db
-    charset: 'utf-8'
+    password: 'admin', //tbd
+    charset: 'utf-8',
   }
 });
 
 var bookshelf = require('bookshelf')(knex);
+
+// potential code create server
+// see http://mherman.org/blog/2015/02/12/postgresql-and-nodejs/#.VwgFNhMrJTY for more
+// var pg = require('pg');
+
+// // connect postgres
+// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+// var client = new pg.Client(connectionString);
+// client.connect();
 
 module.exports = bookshelf;
