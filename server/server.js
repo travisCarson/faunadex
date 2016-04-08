2e25c4862c9ebd2a59f4c110f6464ce3551b9185
Hack Reactor Students <accounts@hackreactor.com>
2016-04-08 11:39:53 -0700

    Fix requirements for models

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+
+var app = express();
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
