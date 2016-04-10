485893af00c8f2f37f0ad47c686ddfabab63c69b
Will Wheeler <willfulbard@gmail.com>
2016-04-10 11:23:22 -0700

    Add drops and creates to schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS faunadex;
+USE faunadex;
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
   username varchar(25) NOT NULL,
@@ -7,18 +11,21 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS users_friends;
 CREATE TABLE users_friends (
   id int NOT NULL AUTO_INCREMENT REFERENCES users,
   userid int NOT NULL AUTO_INCREMENT REFERENCES users,
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS forums;
 CREATE TABLE forums (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(25) NOT NULL,
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
   id int NOT NULL AUTO_INCREMENT,
   userid int NOT NULL REFERENCES users,
@@ -31,6 +38,7 @@ CREATE TABLE posts (
   -- A single event (like a hike) may have many encounters
 -- We may create an events table later which will have a relation
   -- of events to encounters 1:many
+DROP TABLE IF EXISTS encounters;
 CREATE TABLE encounters (
   id int NOT NULL AUTO_INCREMENT,
   userid int NOT NULL REFERENCES users,
@@ -43,6 +51,7 @@ CREATE TABLE encounters (
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS pictures;
 CREATE TABLE pictures (
   id int NOT NULL AUTO_INCREMENT,
   encounterid int NOT NULL REFERENCES encounters,
