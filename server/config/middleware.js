5058d11614f814aec2b6296a01cd4d9a2fd7dc92
Will Wheeler <willfulbard@gmail.com>
2016-04-11 15:28:47 -0700

    Add session to middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,5 +2,14 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 module.exports = function (app, express) {
-	app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.set('trust proxy', 1); // trust first proxy 
+  app.use(session({
+    secret: 'but there isnt a bunny anywhere',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
 }
+
