a954fa9144754ec9017781cad757489efe19e372
Will Wheeler <willfulbard@gmail.com>
2016-04-11 11:14:06 -0700

    Add ip address to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var webpack = require('webpack');
+var ip = require('ip').address();
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-dev-server/client?http://' + ip + ':8080',
     'webpack/hot/only-dev-server',
     './client/index.js'
   ],
