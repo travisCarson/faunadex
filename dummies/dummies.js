d759167cc05dee5c0a948c737545fc7a94dbc003
Will Wheeler <willfulbard@gmail.com>
2016-04-11 17:28:54 -0700

    Change posttime to postTime and encountertime to encounterTime

--- dummies/dummies.js
+++ dummies/dummies.js
@@ -24,8 +24,8 @@ exports.dummyObjEncounters = {
   	title: 'Grey Squirrel',
   	description: 'today i encountered a squirrel. luckily, he was not aggressive',
     location: 'the park',
-    encountertime: '2016-04-03 22:22',
-    posttime: '2016-04-03 22:22',
+    encounterTime: '2016-04-03 22:22',
+    postTime: '2016-04-03 22:22',
     // how do we access the photos? don't have a collection setup for that
     // photo: 'http://i.telegraph.co.uk/multimedia/archive/02845/potd-squirrel_2845650b.jpg',
   },
@@ -33,8 +33,8 @@ exports.dummyObjEncounters = {
     title: 'Elephant',
     description: 'why is there an elephant around here?',
     location: 'market',
-    encountertime: '2016-04-04 12:22',
-    posttime: '2016-04-06 22:22',
+    encounterTime: '2016-04-04 12:22',
+    postTime: '2016-04-06 22:22',
     // how do we access the photos? don't have a collection setup for that
     // photo: 'http://i.telegraph.co.uk/multimedia/archive/02845/potd-squirrel_2845650b.jpg',
   },
@@ -42,8 +42,8 @@ exports.dummyObjEncounters = {
     title: 'Grey African Parrot',
     description: 'it landed on my head!',
     location: 'Hack Reactor',
-    encountertime: '2016-04-07 04:22',
-    posttime: '2016-04-07 22:22',
+    encounterTime: '2016-04-07 04:22',
+    postTime: '2016-04-07 22:22',
     // how do we access the photos? don't have a collection setup for that
     // photo: 'http://i.telegraph.co.uk/multimedia/archive/02845/potd-squirrel_2845650b.jpg',
   }
@@ -55,8 +55,8 @@ exports.dummyEncounters = [
     title: 'Grey Squirrel',
     description: 'today i encountered a squirrel. luckily, he was not aggressive',
     location: 'the park',
-    encountertime: '2016-04-03 22:22',
-    posttime: '2016-04-03 22:22',
+    encounterTime: '2016-04-03 22:22',
+    postTime: '2016-04-03 22:22',
     // how do we access the photos? don't have a collection setup for that
     // photo: 'http://i.telegraph.co.uk/multimedia/archive/02845/potd-squirrel_2845650b.jpg',
   },
@@ -64,8 +64,8 @@ exports.dummyEncounters = [
     title: 'Elephant',
     description: 'why is there an elephant around here?',
     location: 'market',
-    encountertime: '2016-04-04 12:22',
-    posttime: '2016-04-06 22:22',
+    encounterTime: '2016-04-04 12:22',
+    postTime: '2016-04-06 22:22',
     // how do we access the photos? don't have a collection setup for that
     // photo: 'http://i.telegraph.co.uk/multimedia/archive/02845/potd-squirrel_2845650b.jpg',
   },
@@ -73,9 +73,9 @@ exports.dummyEncounters = [
     title: 'Grey African Parrot',
     description: 'it landed on my head!',
     location: 'Hack Reactor',
-    encountertime: '2016-04-07 04:22',
-    posttime: '2016-04-07 22:22',
+    encounterTime: '2016-04-07 04:22',
+    postTime: '2016-04-07 22:22',
     // how do we access the photos? don't have a collection setup for that
     // photo: 'http://i.telegraph.co.uk/multimedia/archive/02845/potd-squirrel_2845650b.jpg',
   }
-];
+
