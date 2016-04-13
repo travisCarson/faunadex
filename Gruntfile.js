d3a5fc9fceaac01518fecdbfbb607fc46d6cd261
Will Wheeler <willfulbard@gmail.com>
2016-04-13 15:07:53 -0700

    Add grunt tasks to run server so no more webpack-dev-server

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+  var webpackConfig = require('./webpack.config.js');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      scripts: {
+        files: 'client/**/*.js',
+        tasks: ['webpack']
+      }
+    },
+    webpack: {
+      options: webpackConfig,
+      faunadex: {
+        entry: './client/index.js',
+        output: {
+          path: __dirname + '/client',
+          filename: 'bundle.js'
+        },
+
+        stats: {
+          colors: true,
+          modules: true,
+          reasons: true
+        },
+
+        watch: false,
+        progress: true
+        
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'server/server.js',
+        watch: ['server']
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-webpack-without-server');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('default', '', function () {
+    var taskList = ['concurrent', 'nodemon', 'watch'];
+    grunt.task.run(taskList);
+  });
+
+
+};
