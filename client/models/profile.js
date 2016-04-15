583dae8ca7f7651092fc4f75c8c6b34bcb9117c9
Will Wheeler <willfulbard@gmail.com>
2016-04-15 12:46:49 -0700

    Remove models folder and all things in it

--- client/models/profile.js
+++
@@ -1,8 +0,0 @@
-var Profile = Backbone.Model.extend({
-  defaults : {
-    username: '',
-    password: '',
-    avatar: '', //path (most likely a url) to an image file
-    description: '' //short user autobiography
-  }
-});
