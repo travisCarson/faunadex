eec452b8f3225e1878a6c1c09f11bf2d3edbd613
Will Wheeler <willfulbard@gmail.com>
2016-04-14 19:23:23 -0700

    Get basic auth tests working

--- test/03-Auth/basic.js
+++ test/03-Auth/basic.js
@@ -1,10 +1,8 @@
 var chai = require('chai');
 var expect = chai.expect;
 chai.should();
-var request = require('supertest');
+var request = require('request');
 
-
-var db = require('../../server/config/db.js');
 var User = require('../../server/models/user.js');
 var app = require('../../server/server.js');
 
@@ -12,6 +10,7 @@ var app = require('../../server/server.js');
 var Site = function() {
   this.username = '';
   this.password = '';
+  this.req = request.defaults({jar: true, followAllRedirects: true});
 };
 
 Site.prototype.post = function (uri, callback) {
@@ -19,56 +18,87 @@ Site.prototype.post = function (uri, callback) {
     username: this.username,
     password: this.password
   };
-  request.agent(app).post(uri).send(data).withCredentials().redirects(1).expect(200).end(callback);
-}
+  // request.agent(app).post(uri).send(data).withCredentials().redirects(1).expect(200).end(callback);
+  this.req.post({ url: 'http://localhost:1337' + uri, form: data}, callback);
+};
+
+Site.prototype.get = function (uri, callback) {
+  this.req.get({ url: 'http://localhost:1337' + uri }, callback);
+};
 
 var site = new Site();
 
 //Delete our user if he already exists
-new User({ username: 'bob123456', password: 'bob' }).fetch().then (function (user) {
+new User({ username: 'bob123456' }).fetch().then (function (user) {
   if (user) {
     user.destroy();
   }
 });
 
+
 //Create a user that we will delete at the end
 var bob = new User({ username: 'bob123456', password: 'bob' }).save();
 
 describe('Basic Authentication', function () {
-  describe('/api/user/signin', function () {
+  describe('/api/user/signin and /api/user/isloggedin', function () {
     
     it('will not 404 when POST to /api/user/signin', function (done) {
       site.username = 'nancy';
-      site.post('/api/user/signin', function(error, res) {
-        expect(error).to.equal(null);
+      site.post('/api/user/signin', function(error, res, body) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('/api/user/isloggedin should return false when not logged in', function (done) {
+      site.get('/api/user/isloggedin', function(error, res, body) {
+        body.should.equal('false');
         done();
       });
     });
 
     it('will not sign a user in with improper credentials, and redirect to login page', function (done) {
       site.username = 'bob123456';
-      site.post('/api/user/signin', function(error, res) {
+      site.password = '';
+      site.post('/api/user/signin', function(error, res, body) {
         expect(error).to.equal(null);
-        request.agent(app).get('/#/dashboard').expect(200).end(function(err, res) {
-          res.text.should.include('Sign In');
+        site.get('/api/user/isloggedin', function(error, res, body) {
+          body.should.equal('false');
           done();
-        })
+        });
       });
     });
 
     it('will sign a user in with proper credentials', function (done) {
       site.username = 'bob123456';
       site.password = 'bob';
-      site.post('/api/user/signin', function(error, res) {
+      site.post('/api/user/signin', function(error, res, body) {
+        var jsonBody = JSON.parse(body);
         expect(error).to.equal(null);
-        request.agent(app).get('/#/dashboard').expect(200).end(function(err, res) {
-          res.text.should.not.include('Sign In');
-          done();
-        })
+        expect(jsonBody.username).to.equal('bob123456');
+        done();
       });
     });
 
-  }); //END describe('Basic REST API endpoints' function () {
+    it('/api/user/isloggedin should return true when logged in', function (done) {
+      site.get('/api/user/isloggedin', function(error, res, body) {
+        body.should.equal('true');
+        done();
+      });
+    });
+  }); 
 
-}); //END describe('Basic Server Functions', function () {
+  describe('/api/user/signout', function() {
+    it('should sign a user out', function(done) {
+      site.get('/api/user/signout', function(error, res, body) {
+        expect(error).to.equal(null);
+        site.get('/api/user/isloggedin', function(error, res, body) {
+          body.should.equal('false');
+          done();
+        });
+      });
+    });
+  });
+
+}); 
 
