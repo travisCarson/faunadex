973e7b36432cbbb5403f527bd29aaf0e77199bf1
Alexander James Turinske <a.turinske@gmail.com>
2016-04-15 12:26:21 -0700

    Add many-to-one Bookshelfjs relationship btwn Encounter and User model

--- server/models/encounter.js
+++ server/models/encounter.js
@@ -1,9 +1,13 @@
 var db = require('../config/db.js');
 var bookshelf = require('bookshelf');
+var User = require('./user');
 
 var Encounter = db.Model.extend({
 	tableName: 'encounters',
-
+  
+  user: function() {
+    return this.belongsTo(User, 'userid');
+  }
 });
 
 module.exports = Encounter;
