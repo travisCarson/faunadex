b2c616749ff3896ee88035c95d883f0f27bb3cf9
Will Wheeler <willfulbard@gmail.com>
2016-04-16 10:20:36 -0700

    Partial fix for auth

--- client/lib/auth.js
+++ client/lib/auth.js
@@ -8,27 +8,12 @@ module.exports = {
       if (data.username) {
         callback(null, data);
       } else {
-        callback(new Error('Not Logged In'));
+         callback(new Error('Not Logged In'));
       }
     })
     .fail((jqXHR, msg) => {
       callback(new Error(msg));
     });
-  },
-
-  isSignedIn: () => {
-    $.get('/api/user/issignedin')
-    .done((data) => {
-      return data;
-    })
-    .fail((jqXHR, msg) => {
-      return false;
-    });
   }
-
-
-
-
-
 
 };
