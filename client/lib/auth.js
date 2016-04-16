7b794801a166d035ef751cc604f8c4ca461ad5d5
Will Wheeler <willfulbard@gmail.com>
2016-04-16 14:52:02 -0700

    Set/unset tokens on login/logout

--- client/lib/auth.js
+++ client/lib/auth.js
@@ -1,11 +1,13 @@
 module.exports = {
   login: (email, password, callback) => {
     if (this.isSignedIn()) {
+      console.log('already signed in');
       return callback(null, true);
     }
     $.post('/api/user/signin', {username: username, password: password})
     .done((data) => {
       if (data.username) {
+        window.localStorage.setItem('com.faunadex', data.token);
         callback(null, data);
       } else {
          callback(new Error('Not Logged In'));
@@ -14,6 +16,16 @@ module.exports = {
     .fail((jqXHR, msg) => {
       callback(new Error(msg));
     });
+  },
+
+  isSignedIn: function() {
+    return !!$window.localStorage.getItem('com.faunadex');
+  },
+
+  signOut: function(callback) {
+    window.localStorage.removeItem('com.faunadex');
+    $.get('/api/user/signout', callback);
   }
+
 
 };
