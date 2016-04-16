5fc0c134122f16acb404c2fee1cb650f474aa278
Alexander James Turinske <a.turinske@gmail.com>
2016-04-15 18:45:54 -0700

    Add plugin registry to Bookshelf database to resolve dependency bug
    When connecting multiple databases using the relation functions (e.g. hasMany, belongsTo, etcetera), it is easy to cause circular dependencies. This registry plugin registers the models in a central location to avoid this issue.

--- server/config/db.js
+++ server/config/db.js
@@ -10,13 +10,8 @@ var knex = require('knex')({
 
 var bookshelf = require('bookshelf')(knex);
 
-// potential code create server
-// see http://mherman.org/blog/2015/02/12/postgresql-and-nodejs/#.VwgFNhMrJTY for more
-// var pg = require('pg');
-
-// // connect postgres
-// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-// var client = new pg.Client(connectionString);
-// client.connect();
+// Resolve circular dependencies of models with the plugin below
+// See https://github.com/tgriesser/bookshelf/wiki/Plugin:-Model-Registry for more information
+bookshelf.plugin('registry');
 
 module.exports = bookshelf;
