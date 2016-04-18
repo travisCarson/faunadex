a437db9c0c66136bf3bb523b457776b2eede2beb
Will Wheeler <willfulbard@gmail.com>
2016-04-18 12:05:46 -0700

    Remove some needless comments and move the route

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,15 +5,6 @@ var encounterUtils = require('../utils/encounterUtils.js');
 module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
 
-  // user routing
-  app.post('/api/user/signup', userUtils.createUser);
-
-  /* 
-
-  Done but not tested: check that createSession actually signs in a user. I don't think it does. I don't think there is a signin function 
-
-   */
-
   app.post('/api/user/signin', userUtils.signInUser);
   app.get('/api/user/signout', userUtils.signOutUser);
   app.post('/api/user/signup', userUtils.createUser);
