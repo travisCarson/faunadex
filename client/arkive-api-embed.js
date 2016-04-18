7754625fe0b0c8829b2db7ea84302442ee3a2617
Alexander James Turinske <a.turinske@gmail.com>
2016-04-17 18:47:26 -0700

    Remove indent

--- client/arkive-api-embed.js
+++ client/arkive-api-embed.js
@@ -8,20 +8,21 @@ var arkiveEmbedCallback = function(data) {
   var iframeAttr;
   var iframeCreation = '<iframe className="frame" name="widget" src ="#" width="100%" height="1" marginheight="0" marginwidth="0" frameborder="no"></iframe>';
   var iframe = window.location.protocol + "//" + (data.results[0].url);
-  if (data.error != 'null') {
-      var $fauna = $('<div></div>');
-      $fauna.attr('class', 'iframe');
-      $fauna.html(iframeCreation);
-      // use the below to add only one iframe to each encounter
-      $('.encounter .animal').each(function(index) {
-        if (!$(this).has('.iframe').length && start) {
-          $(this).append($fauna);
-          start = false;
-          iframeAttr = $('iframe', this)[0];
-        }
-      });
-      iframeAttr.height = arkiveApiHeight;
-      iframeAttr.width = arkiveApiWidth + 22;
-      iframeAttr.src = iframe;
-    }
+  if (data.error !== 'null') {
+    var $fauna = $('<div></div>');
+    $fauna.attr('class', 'iframe');
+    $fauna.html(iframeCreation);
+    // use the below to add only one iframe to each encounter
+    // TODO make below unnecessary
+    $('.encounter .animal').each(function(index) {
+      if (!$(this).has('.iframe').length && start) {
+        $(this).append($fauna);
+        start = false;
+        iframeAttr = $('iframe', this)[0];
+      }
+    });
+    iframeAttr.height = arkiveApiHeight;
+    iframeAttr.width = arkiveApiWidth + 22;
+    iframeAttr.src = iframe;
+  }
 };
