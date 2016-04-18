a1ecb69e33c26dfd3f786c271cac806c7b542e86
Alexander James Turinske <a.turinske@gmail.com>
2016-04-18 12:12:49 -0700

    Add documentation for the server routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,22 +5,22 @@ var encounterUtils = require('../utils/encounterUtils.js');
 module.exports = function (app, express) {
   app.use(express.static(path.join(__dirname, '../../client')));
 
+  // user routing
   app.post('/api/user/signin', userUtils.signInUser);
   app.get('/api/user/signout', userUtils.signOutUser);
   app.post('/api/user/signup', userUtils.createUser);
+  // route keeps a user signed in even if they navigate away from the page
   app.post('/api/user/getsignedinuser', userUtils.getSignedInUser);
 
-  // allow viewing of a user's profile
-  // app.get('/api/:userName', userUtils.fetchUserProfile);
-
   // encounter routing
+  // retrieves the encounters for a user
   app.get('/api/user/encounters/:userName', userUtils.authenticationRequired, encounterUtils.showAllEncountersFromUser);
+  // creates a new encounter
   app.post('/api/user/encounter', userUtils.authenticationRequired, encounterUtils.createEncounter);
+  // retrieves the most recent encounters from the site
   app.get('/api/recentencounters', userUtils.authenticationRequired, encounterUtils.recentEncounters);
+  // retrieves a single encounter
   app.post('/api/encounter', encounterUtils.retrieveEncounterById);
-
-  // app.post('/api/searchanimal', someController);
-  // app.get('/api/friends/recentactivity', someController)
 };
 
 
