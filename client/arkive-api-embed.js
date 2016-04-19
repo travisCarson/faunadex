96b1163fad3b092a852cd1d2b9e7ae0feb947ab9
Alexander James Turinske <a.turinske@gmail.com>
2016-04-18 17:13:47 -0700

    Move ARKive iframe calls to EncounterDetails

--- client/arkive-api-embed.js
+++ client/arkive-api-embed.js
@@ -6,23 +6,15 @@ var arkiveApiWidth = 320;
 var arkiveApiHeight = 355;
 
 var arkiveEmbedCallback = function(data) {
-  var start = true;
-  var iframeAttr;
-  var iframeCreation = '<iframe className="frame" name="widget" src ="#" width="100%" height="1" marginheight="0" marginwidth="0" frameborder="no"></iframe>';
+  var iframeCreation = '<iframe id="frame" name="widget" src ="#" width="100%" height="1" marginheight="0" marginwidth="0" frameborder="no"></iframe>';
   var iframe = window.location.protocol + "//" + (data.results[0].url);
   if (data.error !== 'null') {
     var $fauna = $('<div></div>');
     $fauna.attr('class', 'iframe');
     $fauna.html(iframeCreation);
-    // use the below to add only one iframe to each encounter
-    // TODO make below unnecessary
-    $('.encounter .animal').each(function(index) {
-      if (!$(this).has('.iframe').length && start) {
-        $(this).append($fauna);
-        start = false;
-        iframeAttr = $('iframe', this)[0];
-      }
-    });
+    $('.encounter-details').append($fauna);
+    var iframeAttr = parent.document.getElementById('frame');
+    console.log(parent.document.getElementById('frame'));
     iframeAttr.height = arkiveApiHeight;
     iframeAttr.width = arkiveApiWidth + 22;
     iframeAttr.src = iframe;
